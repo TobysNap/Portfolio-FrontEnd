1bd5d5216480469ae7f24648990ff8e751318563
TobysNap <naptobys17@gmail.com>
2022-10-10 13:27:37 -0300

    Edición componentes Angular

--- PortfolioAngular/PortfolioAngular/src/app/app.module.ts
+++ PortfolioAngular/PortfolioAngular/src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
 import { MenuComponent } from './componentes/menu/menu.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
-    MenuComponent
+    MenuComponent,
+    ProyectosComponent,
+    HabilidadesComponent,
+    EducacionComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
